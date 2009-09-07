fb6e633367905a9b172845c23a36168badb26564
Alex Eagle <alexeagle@google.com>
2009-09-07 15:18:16 -0700

    Add a block inside the method, which can hold expressions.

--- src/main/scala/noop/model/Method.scala
+++ src/main/scala/noop/model/Method.scala
@@ -6,8 +6,9 @@ import collection.mutable.{ArrayBuffer, Buffer}
 /**
  * Represents the declaration of a method in source code.
  */
-class Method(nm: String, rtnTp: String) {
+class Method(nm: String, rtnTp: String, blk: Block) {
   val name: String = nm;
   val returnType: String = rtnTp;
+  val block: Block = blk;
   var parameters: Buffer[Parameter] = new ArrayBuffer[Parameter]();
 }
