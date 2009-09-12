dc674d812aadf6f96bd29bda1a1aca71af94c7e5
Alex Eagle <alexeagle@google.com>
2009-09-12 13:06:13 -0700

    Add a no-args constructor for MockClassLoader, which is more convenient from tests.

--- src/test/scala/noop/interpreter/InterpreterSpec.scala
+++ src/test/scala/noop/interpreter/InterpreterSpec.scala
@@ -1,16 +1,13 @@
 package noop.interpreter
 
-import scala.Console
 import org.scalatest.Spec
 import org.scalatest.matchers.ShouldMatchers
-import noop.grammar.Parser
-import java.io.ByteArrayOutputStream
 
 class InterpreterSpec extends Spec with ShouldMatchers {
-  
+
   describe("the interpreter") {
     it("should register Console as a native type") {
-      val classLoader = new MockClassLoader(new Parser(), List());
+      val classLoader = new MockClassLoader();
       val interpreter = new Interpreter(classLoader);
       interpreter.registerNativeTypes;
       classLoader.cache should contain key("Console");
