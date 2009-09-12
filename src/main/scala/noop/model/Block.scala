b15c91cd9c048098dd97b88e4744ae19e8aa9b67
Alex Eagle <alexeagle@google.com>
2009-09-12 12:57:14 -0700

    Add a native block, which extends from Block but is provided a function from Context to Option[NoopObject]. The function is called to evaluate the block, rather than iterating through the statements.

--- src/main/scala/noop/model/Block.scala
+++ src/main/scala/noop/model/Block.scala
@@ -1,11 +1,14 @@
 package noop.model
 
-
 import collection.mutable.{Buffer, ArrayBuffer}
+import interpreter.Context
+import types.NoopObject
 
 /**
  * @author alexeagle@google.com (Alex Eagle)
  */
-class Block {
+class Block extends Expression {
   var statements: Buffer[Expression] = new ArrayBuffer[Expression]();
+
+  def evaluate(c: Context): Option[NoopObject] = None;
 }
