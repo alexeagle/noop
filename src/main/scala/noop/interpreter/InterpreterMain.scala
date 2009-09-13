4a4f8db5792e90b3236c1b7e1e4082ef95def212
Alex Eagle <alexeagle@google.com>
2009-09-13 09:34:40 -0700

    Add copyrights

--- src/main/scala/noop/interpreter/InterpreterMain.scala
+++ src/main/scala/noop/interpreter/InterpreterMain.scala
@@ -1,11 +1,22 @@
+/**
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package noop.interpreter;
 
-import scala.collection.mutable.{ArrayBuffer, Stack, Map};
 import grammar.Parser;
-import model.{Block, ClassDefinition, Expression, MethodInvocationExpression, NativeExpression, Parameter, StringLiteralExpression};
-import model.Method;
-import org.antlr.runtime.MismatchedTokenException;
-import types.{NoopConsole, NoopObject};
 
 /**
  * The static entry point into the Noop interpreter, for use from the command-line.
