d09f455313f91637381c96c0c532787082e4655f
Alex Eagle <alexeagle@google.com>
2009-09-13 09:34:40 -0700

    Add copyrights

--- src/main/scala/noop/grammar/Parser.scala
+++ src/main/scala/noop/grammar/Parser.scala
@@ -1,7 +1,23 @@
+/**
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package noop.grammar;
 
 import java.io.InputStream;
-import noop.model.{Block, File};
+import noop.model.{File};
 import noop.grammar.antlr.NoopAST;
 import noop.grammar.antlr.NoopParser;
 import noop.grammar.antlr.NoopLexer;
@@ -9,7 +25,7 @@ import noop.grammar.antlr.NoopLexer;
 import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.RecognitionException;
+
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
 
--- src/test/scala/noop/types/StringSpec.scala
+++ src/test/scala/noop/types/StringSpec.scala
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package noop.types
 
 import grammar.Parser
@@ -11,14 +27,21 @@ import org.scalatest.Spec
  */
 
 class StringSpec extends Spec with ShouldMatchers {
+  def createFixture = {
+    val stringSourcePath = new File(getClass().getResource("/stdlib").toURI).getAbsolutePath();
+    new ClassLoader(new Parser(), List(stringSourcePath))
+  }
 
   describe("a Noop String") {
     it("should have a valid class definition parsed from Noop source") {
-      val stringSourcePath = new File(getClass().getResource("/stdlib").toURI).getAbsolutePath();
-      println(stringSourcePath);
-      val classLoader = new ClassLoader(new Parser(), List(stringSourcePath));
+      val classLoader = createFixture;
       val classDef = classLoader.findClass("String");
       classDef.name should be("String");
+    }
+
+    it("should have a native implementation of the length method") {
+      val classLoader = createFixture;
+
     }
   }
 }
