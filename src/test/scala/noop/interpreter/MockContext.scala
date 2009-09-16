978188bf62dab775a6fb6bdcab9ec6b602083675
Alex Eagle <alexeagle@google.com>
2009-09-16 11:28:22 -0700

    Evaluate the identifier declaration expression

---
+++ src/test/scala/noop/interpreter/MockContext.scala
@@ -0,0 +1,26 @@
+package noop.interpreter
+
+
+import collection.mutable.Stack
+import model.{Method, Modifier, ClassDefinition}
+
+/**
+ * @author alexeagle@google.com (Alex Eagle)
+ */
+
+trait MockContext {
+  def fixture = {
+    val stack = new Stack[Frame];
+    val classLoader = new MockClassLoader();
+    val classDefinition = new ClassDefinition();
+    classDefinition.name = "String";
+    val method = new Method("length", "Int", null);
+    method.modifiers += Modifier.native;
+    classDefinition.methods += method;
+
+    classLoader.classes += Pair("String", classDefinition);
+    classLoader.classes += Pair("Int", new ClassDefinition());
+    val context = new Context(stack, classLoader);
+    context;
+  }
+}
