70e66ec3b1b6c351475cb2df35aac646ae439568
Jeremie Lenfant-Engelmann <tocman@gmail.com>
2009-09-17 22:49:29 -0700

    Starting the work on the if statement. We can parse simple equality expression (not creating the model objects yet) and that's about it.

---
+++ src/test/scala/noop/grammar/IfSpec.scala
@@ -0,0 +1,47 @@
+package noop.grammar;
+
+import org.scalatest.matchers.ShouldMatchers;
+import org.scalatest.Spec;
+
+class IfSpec extends Spec with ShouldMatchers {
+
+  val parser = new Parser();
+
+  describe("an if expression") {
+
+    it("should parse correctly equality expression on two literals") {
+      val source = "{ if (1 == 1) { } }";
+      val ifBlock = parser.parseBlock(source);
+
+      ifBlock.toStringTree() should be("(IF 1 == 1)");
+    }
+
+    it("should parse correctly an inequality expression on two literals") {
+      val source = "{ if (1 != 2) { } }";
+      val ifBlock = parser.parseBlock(source);
+
+      ifBlock.toStringTree() should be("(IF 1 != 2)");
+    }
+
+    it("should parse correctly an inequality expression followed by an or'ed equality expression on literals") {
+      val source = "{ if (1 != 2 || 1 == 1) { } }";
+      val ifBlock = parser.parseBlock(source);
+
+      ifBlock.toStringTree() should be("(IF 1 != 2 || 1 == 1)");
+    }
+
+    it("should parse correctly an inequality expression followed by an and'ed equality expression on literals") {
+      val source = "{ if (1 != 2 && 1 == 1) { } }";
+      val ifBlock = parser.parseBlock(source);
+
+      ifBlock.toStringTree() should be("(IF 1 != 2 && 1 == 1)");
+    }
+
+    it("should parse correctly complex conditional with or and and primaries") {
+      val source = "{ if (1 != a.getValue() && \"hello\" != \"wolrd\" || 50 == myVar) { } }";
+      val ifBlock = parser.parseBlock(source);
+
+      ifBlock.toStringTree() should be("(IF 1 != (. a getValue ARGS) && \"hello\" != \"wolrd\" || 50 == myVar)");
+    }    
+  }
+}
