c09e4707d3aaeb83044dd13ebe2309ab11d5b6f8
Alex Eagle <aeagle22206@gmail.com>
2009-11-21 16:13:08 -0800

    Fix the collisions in the uber-jar. Instead of trying to build the interpreter as a single executable jar, I think it's time to have a startup script and keep all the original jars in a lib directory. It should be easier to launch the interpreter now.

--- findMissingCopyright.sh
+++ findMissingCopyright.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for root in compiler core interpreter
   do
-  for i in `find $root -name "*.scala"`
+  for i in $(find $root -name "*.scala")
     do
     head -2 $i | tail -1 | grep -v Copyright && echo $i
     done
