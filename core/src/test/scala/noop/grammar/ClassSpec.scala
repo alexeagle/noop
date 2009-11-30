069776dd6060d8059d47974de8eec905d0ae37e1
Alex Eagle <alexeagle@google.com>
2009-11-30 08:13:22 -0800

    Propagate modifiers on parameters from grammar to the model

--- core/src/test/scala/noop/grammar/ClassSpec.scala
+++ core/src/test/scala/noop/grammar/ClassSpec.scala
@@ -79,6 +79,17 @@ class ClassSpec extends Spec with ShouldMatchers {
       parser.parseFile(source).toStringTree() should equal (
           "(CLASS Bar (PARAMS (PARAM (MOD mutable) A a) (PARAM (MOD delegate) B b) " +
           "(PARAM (MOD mutable delegate) C c)))");
+      val file = parser.file(source);
+      file.classDef.name should be("Bar");
+      file.classDef.parameters should have length(3);
+      file.classDef.parameters(0).modifiers should have length(1);
+      file.classDef.parameters(0).modifiers should contain(Modifier.mutable);
+      file.classDef.parameters(1).modifiers should have length(1);
+      file.classDef.parameters(1).modifiers should contain(Modifier.delegate);
+      file.classDef.parameters(2).modifiers should have length(2);
+      file.classDef.parameters(2).modifiers should contain(Modifier.mutable);
+      file.classDef.parameters(2).modifiers should contain(Modifier.delegate);
+
     }
 
     it("should allow an implements clause with one interface") {
